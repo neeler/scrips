9c326e4e40df7d43a3a08cbbc4b671b7a7d79e45
Neel Rao <neelorao@gmail.com>
2025-02-10 22:17:21 -0800

    feat: semantic release (#1)

---
+++ release.config.js
@@ -0,0 +1,85 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+module.exports = {
+    branches: ['main'],
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+                releaseRules: [
+                    // Major Releases are for Breaking Changes only!
+                    { breaking: true, release: 'major' },
+
+                    // Minor Releases:
+                    { type: 'feat', release: 'minor' },
+                    { type: 'feature', release: 'minor' },
+
+                    // Patch Releases:
+                    { revert: true, release: 'patch' },
+                    { type: 'revert', release: 'patch' },
+                    { type: 'chore', release: 'patch' },
+                    { type: 'fix', release: 'patch' },
+                    { type: 'perf', release: 'patch' },
+                    { type: 'chore', release: 'patch' },
+                    { type: 'refactor', release: 'patch' },
+                    { type: 'build', release: 'patch' },
+                    { type: 'docs', release: 'patch' },
+                    { type: 'test', release: 'patch' },
+                    { type: 'style', release: 'patch' },
+
+                    // No Releases:
+                    { type: 'ci', release: false },
+                ],
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                presetConfig: {
+                    types: [
+                        { type: 'feat', section: 'Features', hidden: false },
+                        { type: 'feature', section: 'Features', hidden: false },
+                        { type: 'fix', section: 'Bug Fixes', hidden: false },
+                        {
+                            type: 'perf',
+                            section: 'Performance Improvements',
+                            hidden: false,
+                        },
+                        { type: 'revert', section: 'Reverts', hidden: false },
+                        {
+                            type: 'docs',
+                            section: 'Documentation',
+                            hidden: false,
+                        },
+                        { type: 'style', section: 'Styles', hidden: false },
+                        {
+                            type: 'chore',
+                            section: 'Miscellaneous Chores',
+                            hidden: false,
+                        },
+                        {
+                            type: 'refactor',
+                            section: 'Code Refactoring',
+                            hidden: false,
+                        },
+                        { type: 'test', section: 'Tests', hidden: false },
+                        {
+                            type: 'build',
+                            section: 'Build System',
+                            hidden: false,
+                        },
+                        {
+                            type: 'ci',
+                            section: 'Continuous Integration',
+                            hidden: false,
+                        },
+                    ],
+                },
+            },
+        ],
+        '@semantic-release/npm',
+    ],
+};
